64db736133de712319d92f0bb9f5abc25dc49d4d
Jayanth <devupallijayanth@gmail.com>
2025-08-05 23:15:28 +0530

    updated amazon

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -5,14 +5,11 @@ import { loadCart } from "../data/cart.js";
 
 async function loadPage(){
   try{
-    //throw 'error10';
 
     await loadProductsFetch();
 
     const value= await new Promise((resolve,reject)=>{
-      //throw 'error2';
       loadCart(()=>{
-        //reject('error3');
         resolve('value2');
       });
     });
@@ -25,49 +22,3 @@ async function loadPage(){
   renderPaymetSummary();
 }
 loadPage();
-
-//import '../data/cart-class.js';
-//import '../data/backend-practice.js';
-/*
-Promise.all([
-  loadProductsFetch(),
-  new Promise((resolve)=>{
-    loadCart(()=>{
-      resolve('value2');
-    });
-  })
-
-]).then((values)=>{
-  console.log(values);
-  renderOrderSummary();
-  renderPaymetSummary();
-});
-
-*/
-/*
-new Promise((resolve)=> {
-  loadProducts(()=>{
-    resolve('value1');
-  });
-
-}).then((value)=>{
-  return new Promise((resolve)=>{
-    loadCart(()=>{
-      resolve();
-    });
-  });
-
-}).then(()=>{
-  renderOrderSummary();
-  renderPaymetSummary();
-});
-*/
-
-/*
-loadProducts(() => {
-  loadCart(() => {
-    renderPaymetSummary();
-    renderOrderSummary();
-  });
-});
-*/
