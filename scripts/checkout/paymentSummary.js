d8b9c8e650504f8db70e399e6ccfc416d91e20e2
Jayanth <devupallijayanth@gmail.com>
2025-08-07 12:06:59 +0530

    items

--- scripts/checkout/paymentSummary.js
+++ scripts/checkout/paymentSummary.js
@@ -27,7 +27,7 @@ export function renderPaymetSummary(){
     </div>
 
     <div class="payment-summary-row">
-      <div>Items (3):</div>
+      <div>Items:</div>
       <div class="payment-summary-money">
       $${formatCurrency(productPriceCents)}
       </div>
